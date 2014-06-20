420a2db3cca530976e2126cac058f8d070f35a2d
Dan Pickett <dan.pickett@launchware.com>
2014-06-20 15:45:01 -0400

    basic sinatra template

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+configure do
+  set :views, 'app/views'
+end
+
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
+  require file
+end
+
+get '/' do
+  erb :index
+end
